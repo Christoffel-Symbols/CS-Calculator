be2e559cacec4eaa998b8d7a79d5987b4ea69a8d
Dhananjhay <dhananjhay03@gmail.com>
2024-06-02 15:51:53 +0530

    made calculating riemann tensor of second kind independent from riemann tensor of first kind

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ long_description = __doc__.strip()
 
 setup(
     name="PyCSC",
-    version="2.1.1",
+    version="2.1.2",
     description=long_description,
     url="https://github.com/Christoffel-Symbols/Christoffel-Symbols-Calculator",
     author="Dhananjhay Bansal",
